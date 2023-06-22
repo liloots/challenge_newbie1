63e02120809746b27fa19139eb49e34a6492c6ef
Murilo Ruano <115485701+liloots@users.noreply.github.com>
2023-06-21 22:52:00 -0300

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,35 @@
-import './globals.css'
-import { Montserrat, Fraunces } from 'next/font/google'
-import { DefaultBody } from '../components/default-page-layout'
+import "./globals.css";
+import { Montserrat, Fraunces } from "next/font/google";
+import { DefaultBody } from "../components/default-page-layout";
+import StyledComponentsRegistry from "./lib/registry";
 
-const montserrat = Montserrat({ 
-  subsets: ['latin'],
-  weight: ['500', '700']
-})
-const fraunces = Fraunces({ 
-  subsets: ['latin'],
-  weight: ['700']
-})
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["500", "700"],
+});
+const fraunces = Fraunces({
+  subsets: ["latin"],
+  weight: ["700"],
+});
 
 export const metadata = {
-  title: 'Product preview card component',
-  description: 'Product preview card component - Newbie Challenge',
-}
+  title: "Product preview card component",
+  description: "Product preview card component - Newbie Challenge",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <DefaultBody className={montserrat.className}>{children}</DefaultBody>
+      <DefaultBody className={montserrat.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </DefaultBody>
     </html>
-  )
+  );
 }
